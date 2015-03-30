a2a4ae5928569e0b2c7f636e42736e36cb2cf294
jhamski <hamski.2@gmail.com>
2015-03-29 22:20:43 -0400

    Filled in data, imported into R

--- table_import.R
+++ table_import.R
@@ -1,1 +1,19 @@
 # IS607 Homework Week 9
+
+library(XML)
+library(RJSONIO)
+library(RCurl)
+
+#HTML
+html <- readHTMLTable("books.html", header=TRUE, as.data.frame = TRUE)
+str(html)
+
+#XML
+xml <- xmlParse("books.xml")
+xml.DF <- xmlToDataFrame(xmlChildren(xml))
+str(xml.DF)
+
+#JSON
+json <- fromJSON("books.json")
+json.DF <- data.frame(json$Books)
+str(json.DF)
